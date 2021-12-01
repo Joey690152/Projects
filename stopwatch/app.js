83b9943d1dee8abff71a8cfdd5c4b7e14c2e9b19
Joseph Phillipson <joey.phillipson1@hotmail.com>
2021-12-01 12:51:19 +0000

    init

---
+++ stopwatch/app.js
@@ -0,0 +1,15 @@
+const hours = document.getElementsByClassName('.hours')
+const mins = document.getElementsByClassName('.mins')
+const secs = document.getElementsByClassName('.secs')
+const startBtn =document.getElementsByClassName('.start')
+const stopBtn =document.getElementsByClassName('.stop')
+const resetBtn =document.getElementsByClassName('.reset')
+
+let seconds = 0
+let minutes = 0
+let hour = 0
+let interval;
+
+
+
+
